bf02a9f8c86c3728876f55a33114b55e5245593a
Trun.03 <ductrung17102003@gmail.com>
2023-02-27 14:52:14 +0700

    push ex1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,8 +1,5 @@
-import os
-
 print("Input number of students in a class:")
 x =  int(input())
-qw = []
 y = []
 z = []
 n = []
@@ -12,12 +9,10 @@ for i in range(x):
     n.append(i)
     print("Input students",i+1,"ID")
     y[i] = input()  
-    print("INput students",i+1,"names:")
+    print("Input students",i+1,"name:")
     z[i] = input()
     print("Student",i+1,"DoB")
     n[i] = input()
-print(y,z,n)
-
 
 print("Input number of courses in a class:")
 r =  int(input())  
@@ -30,14 +25,36 @@ for t in range(r):
     d[t] = input()
     print("Input course",t+1,"name:")
     q[t] = input()
-print(d,q)
 
-print("Select a course you want to input mark:")
+print("Select a course name you want to input mark for student:")
 e = input()
+b = []
+k = []
 if e in q:
- print("Mark:")
- c = int(input())
+ for i in range(x):
+  b.append(i)
+  print("Input mark for student",i+1,":")
+  b[i]= int(input())
 else:
- print("Please select the course again.")
+  print("Please select the course again.")
+
+print("***************************************************************************************************************************************************************")
+print("                              **                    1. Student's name.                                   **")
+print("                              **                    2. Student's ID.                                     **")
+print("                              **                    3. Student's DOB.                                    **")
+print("                              **                    4. Course's name.                                    **")
+print("                              **                    5. Course's ID.                                      **")
+print("                              **                    6. Select a course to input mark.                    **")
+print("                              **                    7. Input mark for student.                           **")
+print("                              **                    8. Print the lists.                                  **")
+print("****************************************************************************************************************************************************************")
 
 
+for i in range(x):
+ print('\n'"Student",i+1,"ID:",y[i],'\n'"Student",i+1,"name:",z[i],'\n'"Student",i+1,"DOB:",n[i])
+for t in range(r):
+ print('\n'"Course",t+1,"ID:",d[t],'\n'"Course",t+1,"name",q[t])
+
+print('\n'"Student in course",q[t])
+for i in range(x):
+  print("Student",i+1,"mark:",b[i])
