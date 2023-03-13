bf20422e613637872d12ce3123c08a8275838add
Trun.03 <ductrung17102003@gmail.com>
2023-03-14 00:30:13 +0700

    push ex2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -33,31 +33,35 @@ class courses:   #Class for course's information
      print("Course",t+1,"ID:", course.id)
      print("Course",t+1,"Name:", course.name)
 listcourse = []
+CoursesNAME = []
 for t in range(r):
+    CoursesNAME.append(t)
     print("Input course",t+1,"id:")
     CoursesID = input()
     print("Input course",t+1,"name:")
-    CoursesNAME = input()
-    CoursesINFO = courses(CoursesID, CoursesNAME)
+    CoursesNAME[t] = input()
+    CoursesINFO = courses(CoursesID, CoursesNAME[t])
     listcourse.append(CoursesINFO)
-
 print("Select a course name you want to input mark for student:")
 e = input()
 class mark:  #class for input mark
   def __init__(mark, b):
     mark.number = b
-  def studentmark(mark):
-    print("studen",i+1,"mark:", mark.number)
-Marklist = []
-if e in CoursesNAME:
- for i in range(x):
-  print("Input mark for student",i+1,":")
-  MARK = int(input())
-  Studentmark = mark(MARK)
-  Marklist.append(Studentmark)
-else:
-  print("Please select the course again.")
 
+  def studentmark(mark):
+    print("student",i+1,"mark:",mark.number)
+Marklist = []
+
+while e not in CoursesNAME:
+  print("Please select the course again.")
+  e = input()
+
+for i in range(x):
+       print("Input mark for student",i+1,":")
+       marks = int(input())
+       Studentmark = mark(marks)
+       Marklist.append(Studentmark)
+    
 print('\n')
 print("*******************************")
 print("*** 1.Student's Information ***")
@@ -77,7 +81,6 @@ print("*******************************")
 print(" **    3.Student's mark     ** ")
 print("*******************************")
 print(' ')
-print("Student'S mark in course", CoursesNAME,":")
+print("Student'S mark in course", CoursesNAME[t],":")
 for i in range(x):
   Marklist[i].studentmark()
-
